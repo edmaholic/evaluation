71c942177d88ac02a2706f5ff3096eabbcc73ffb
edmaholic <avi.gupta@st.niituniversity.in>
2019-09-26 10:16:38 +0530

    main file

---
+++ main.java
@@ -0,0 +1,8 @@
+package test;
+class main {
+	public static void main (String args[]) {
+		System.out.println("Sum 1+1 = "+sum.sum(1,1));
+		System.out.println("Sub 3-2 = "+subtract.subtract(3,2));
+		System.out.println("Multiply 4x3 = "+multiply.multiply(4,3));
+	}
+}
