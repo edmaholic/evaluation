1c5fb484e2d6715595b8505f1c8ab7e35c629112
edmaholic <avi.gupta@st.niituniversity.in>
2019-09-26 10:20:28 +0530

    new changes

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 package test;
 class main {
 	public static void main (String args[]) {
-		System.out.println("Sum 1+1 = "+sum.sum(1,1));
-		System.out.println("Sub 3-2 = "+subtract.subtract(3,2));
-		System.out.println("Multiply 4x3 = "+multiply.multiply(4,3));
+		System.out.println("Sum 2+1 = "+sum.sum(2,1));
+		System.out.println("Sub 2-2 = "+subtract.subtract(2,2));
+		System.out.println("Multiply 2x3 = "+multiply.multiply(2,3));
 	}
 }
